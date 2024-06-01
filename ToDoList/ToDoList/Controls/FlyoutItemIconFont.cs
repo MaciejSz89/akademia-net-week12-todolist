80928c66d5d352888e498c9dbbad859c7e35cccd
Maciej Szafranski <maciej.szafranski1@gmail.com>
2024-06-01 21:42:09 +0200

    backup 2024-06-01 FlyoutItem icon changing color

---
+++ ToDoList/ToDoList/Controls/FlyoutItemIconFont.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDoList.Controls
+{
+    public class FlyoutItemIconFont : FlyoutItem
+    {
+        public static readonly BindableProperty IconGlyphProperty = BindableProperty.Create(
+            nameof(IconGlyphProperty),
+            typeof(string),
+            typeof(FlyoutItemIconFont),
+            string.Empty);
+
+        public string IconGlyph
+        {
+            get { return (string)GetValue(IconGlyphProperty); }
+            set { SetValue(IconGlyphProperty, value); }
+        }
+    }
+}
